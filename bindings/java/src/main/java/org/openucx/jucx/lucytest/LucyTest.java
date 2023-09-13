fa6fe58314c23f0f20eb1a092cf8d5ebdaa15e9a
Lucy Ge <lucy.ge@alluxio.com>
2023-09-13 15:30:24 -0700

    worker version of simple runtestclient&runtestserer program

--- bindings/java/src/main/java/org/openucx/jucx/lucytest/LucyTest.java
+++ bindings/java/src/main/java/org/openucx/jucx/lucytest/LucyTest.java
@@ -48,6 +48,9 @@ import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
@@ -56,21 +59,34 @@ import java.util.stream.Stream;
 
 public class LucyTest {
   public static final String RANDOM_TEXT = "ABCDEFG-LUCY";
-  public static final int MEM_SIZE = 1024;
+  public static final int MEM_SIZE = 64;
   public static final String FILE_TO_SEND = "/root/testfile1";
+  public static UcpContext globalContext_;
+
+  public LucyTest() {
+    globalContext_ = new UcpContext(new UcpParams()
+        .requestStreamFeature()
+        .requestTagFeature()
+        .requestWakeupFeature());
+  }
 
   public static void main(String[] args) {
     if (args.length < 1) {
       System.out.println("Need at least one arg");
       System.exit(-1);
     }
-    LucyTest lucyTest = new LucyTest();
-    if (args[0].equalsIgnoreCase("client")) {
-      System.out.println("Start runTestClient...");
-      lucyTest.runTestClient();
-    } else if (args[0].equalsIgnoreCase("server")) {
-      System.out.println("Start runTestServer...");
-      lucyTest.runTestServer();
+    try {
+      LucyTest lucyTest = new LucyTest();
+      if (args[0].equalsIgnoreCase("client")) {
+        System.out.println("Start runTestClient...");
+        lucyTest.runTestClient();
+      } else if (args[0].equalsIgnoreCase("server")) {
+        System.out.println("Start runTestServer...");
+        lucyTest.runTestServer();
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+      return;
     }
     System.exit(-1);
   }
@@ -83,7 +99,6 @@ public class LucyTest {
   }
 
   public void openFile() throws IOException {
-    UcpContext context = new UcpContext(new UcpParams().requestTagFeature());
 //    Path tempFile = Files.createTempFile("jucx", "test");
     Path tempFile = new File(FILE_TO_SEND).toPath();
     // 1. Create FileChannel to file in tmp directory.
@@ -93,13 +108,13 @@ public class LucyTest {
 //    buf.asCharBuffer().put(RANDOM_TEXT);
 //    buf.force();
     // 2. Register mmap buffer with ODP
-    UcpMemory mmapedMemory = context.memoryMap(new UcpMemMapParams()
+    UcpMemory mmapedMemory = globalContext_.memoryMap(new UcpMemMapParams()
         .setAddress(UcxUtils.getAddress(buf)).setLength(MEM_SIZE).nonBlocking());
 
 //    assertEquals(mmapedMemory.getAddress(), UcxUtils.getAddress(buf));
 
     // 3. Test allocation
-    UcpMemory allocatedMemory = context.memoryMap(new UcpMemMapParams()
+    UcpMemory allocatedMemory = globalContext_.memoryMap(new UcpMemMapParams()
         .allocate().setProtection(UcpConstants.UCP_MEM_MAP_PROT_LOCAL_READ)
         .setLength(MEM_SIZE).nonBlocking());
 //    assertEquals(allocatedMemory.getLength(), MEM_SIZE);
@@ -107,7 +122,7 @@ public class LucyTest {
     allocatedMemory.deregister();
     mmapedMemory.deregister();
     fileChannel.close();
-    context.close();
+    globalContext_.close();
   }
 
 
@@ -128,23 +143,21 @@ public class LucyTest {
       acceptedEndpoints_.add(acceptedRemoteEp);
       System.out.println("Connection from client established:"
           + acceptedRemoteEp.getRemoteAddress());
-      LucyTest.sendFile(acceptedRemoteEp);
+      LucyTest.sendFile(worker_, acceptedRemoteEp);
     }
   }
 
 
-  public static void sendFile(UcpEndpoint remoteEp) {
-    UcpContext context = new UcpContext(new UcpParams().requestTagFeature());
+  public static void sendFile(UcpWorker worker,
+                              UcpEndpoint remoteEp) {
     Path tempFile = new File(FILE_TO_SEND).toPath();
     FileChannel fileChannel = null;
     try {
-      fileChannel = FileChannel.open(tempFile, CREATE, WRITE, READ, DELETE_ON_CLOSE);
+      fileChannel = FileChannel.open(tempFile, READ);
       MappedByteBuffer buf = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, MEM_SIZE);
-      UcpMemory mmapedMemory = context.memoryMap(new UcpMemMapParams()
+      UcpMemory mmapedMemory = globalContext_.memoryMap(new UcpMemMapParams()
           .setAddress(UcxUtils.getAddress(buf)).setLength(MEM_SIZE).nonBlocking());
       long tag = getTag(FILE_TO_SEND, 0);
-      ReentrantLock lock = new ReentrantLock();
-      Condition condition = lock.newCondition();
       UcpRequest sentReq = remoteEp.sendTaggedNonBlocking(mmapedMemory.getAddress(), MEM_SIZE,
         tag, new UcxCallback() {
             final long startTime = System.nanoTime();
@@ -152,35 +165,33 @@ public class LucyTest {
             @Override
             public void onSuccess(UcpRequest request) {
               System.out.println("onSuccess! request completed:" + request.isCompleted());
-              condition.signalAll();
             }
 
             @Override
             public void onError(int ucsStatus, String errorMsg) {
               logUtil(errorMsg + ",status:" + ucsStatus, null);
-              condition.signalAll();
             }
           });
-      try {
-        lock.lock();
-        condition.await();
-      } catch (InterruptedException e) {
-        throw new RuntimeException(e);
+      while(!sentReq.isCompleted()) {
+        try {
+          System.out.println("start progressing sentReq");
+          worker.progressRequest(sentReq);
+        } catch (Exception e) {
+          throw new RuntimeException(e);
+        }
       }
       System.out.println("file sent, instant sentReq isCompleted:" + sentReq.isCompleted());
+      mmapedMemory.deregister();
     } catch (IOException e) {
       logUtil("sendFile error", e);
     }
   }
 
-  InetSocketAddress serverAddr_ = new InetSocketAddress("127.0.0.1", 0);
+  InetSocketAddress serverAddr_ = new InetSocketAddress("172.31.21.70", 1234);
   long tagFileIdMask_ = 0xFFFFFFFFFFFF0000L;
 
-  public void runTestServer() {
-    UcpContext context = new UcpContext(new UcpParams()
-        .requestStreamFeature()
-        .requestTagFeature());
-    UcpWorker worker = context.newWorker(new UcpWorkerParams());
+  public void runTestServer() throws Exception {
+    UcpWorker worker = globalContext_.newWorker(new UcpWorkerParams());
     // rdma worker params for rdma Send/Receive with TAG matching?? check UcpEndpointTest.testSendRecv
 //    UcpWorkerParams rdmaWorkerParams = new UcpWorkerParams().requestWakeupRMA();
 
@@ -192,10 +203,41 @@ public class LucyTest {
         .filter(addr -> !addr.isLinkLocalAddress())
         .collect(Collectors.toList());
 
+    CompletableFuture<UcpEndpoint> connectedFut = new CompletableFuture<UcpEndpoint>();
     UcpListenerParams listenerParams = new UcpListenerParams()
-        .setSockAddr(serverAddr_)
-        .setConnectionHandler(new LucyConnectionListener(worker));
-    UcpListener listener = worker.newListener(listenerParams);
+        .setConnectionHandler(new UcpListenerConnectionHandler() {
+          @Override
+          public void onConnectionRequest(UcpConnectionRequest connectionRequest) {
+            UcpEndpoint acceptedRemoteEp = worker.newEndpoint(
+                new UcpEndpointParams()
+                    .setPeerErrorHandlingMode()
+                    .setConnectionRequest(connectionRequest));
+            acceptedEndpoints_.add(acceptedRemoteEp);
+            System.out.println("Connection from client established:"
+                + acceptedRemoteEp.getRemoteAddress());
+            connectedFut.complete(acceptedRemoteEp);
+          }
+        });
+    for (InetAddress addr : addresses) {
+      UcpListener listener = worker.newListener(listenerParams
+          .setSockAddr(new InetSocketAddress(addr, 1234)));
+      System.out.println("Bound UcpListner on: " + listener.getAddress());
+    }
+
+    try {
+      while (worker.progress() == 0) {
+        System.out.println("nothing to progress");
+        worker.waitForEvents();
+      }
+      UcpEndpoint remoteClientEp = connectedFut.get();
+      System.out.println("Accepted remote ep connection, localAddr:"
+          + remoteClientEp.getLocalAddress() + ", remoteAddr:"
+          + remoteClientEp.getRemoteAddress());
+      LucyTest.sendFile(worker, remoteClientEp);
+    } catch (Exception e) {
+      logUtil("exception in runTestServer", e);
+      throw e;
+    }
 /*
     UcpEndpoint clientEndpoint = worker.newEndpoint(
         new UcpEndpointParams()
@@ -239,9 +281,62 @@ public class LucyTest {
             .setPeerErrorHandlingMode()
             .setErrorHandler((ep, status, errorMsg) ->
                 System.out.println("[ERROR] creating ep to remote:"
-                    + serverAddr_ + " errored out: " + errorMsg))
+                    + serverAddr_ + " errored out: " + errorMsg
+                    + " status:" + status + ",ep:" + ep.toString()))
             .setSocketAddress(serverAddr_));
+//    establishOOBConnection(epToServer, worker);
     return epToServer;
+  }
+
+  public boolean sendStream(UcpEndpoint remoteEp,
+                            UcpWorker localWorker,
+                            final long address, final long size,
+                            final boolean blocking) {
+    final UcpRequest request = remoteEp.sendStreamNonBlocking(address, size, null);
+    while (blocking && !request.isCompleted()) {
+      try {
+        System.out.println("sendStream req not complete, keep progress req.");
+        localWorker.progressRequest(request);
+      } catch (Exception e) {
+        // Should never happen, since we do no throw exceptions inside our error handlers
+        throw new IllegalStateException(e);
+      }
+    }
+    return request.isCompleted();
+  }
+
+  public boolean receiveStream(UcpEndpoint remoteEp,
+                                       UcpWorker localWorker,
+                                       final long address, final long size,
+                                       final boolean blocking) {
+    final UcpRequest request = remoteEp.recvStreamNonBlocking(
+        address, size, UcpConstants.UCP_STREAM_RECV_FLAG_WAITALL, null);
+    while (blocking && !request.isCompleted()) {
+      try {
+        localWorker.progressRequest(request);
+      } catch (Exception e) {
+        // Should never happen, since we do no throw exceptions inside our error handlers
+        throw new IllegalStateException(e);
+      }
+    }
+    return request.isCompleted();
+  }
+
+  public void establishOOBConnection(UcpEndpoint remoteEp,
+                                     UcpWorker localWorker) {
+    long dummyAddr = getTag("OOB_ESTABLISH_CONN", 0);
+    ByteBuffer buf = ByteBuffer.allocateDirect(Long.BYTES);
+    buf.putLong(dummyAddr);
+    ByteBuffer recvBuf = ByteBuffer.allocateDirect(Long.BYTES);
+    UcpMemory memory = globalContext_.registerMemory(buf);
+    UcpMemory recvMemory = globalContext_.registerMemory(recvBuf);
+    boolean sendSuccess = sendStream(remoteEp, localWorker, memory.getAddress(), memory.getLength(),true);
+    System.out.println("sendSuccess:" + sendSuccess);
+    boolean recvSuccess = receiveStream(remoteEp, localWorker, recvMemory.getAddress(), recvMemory.getLength(), true);
+    recvBuf.flip();
+    System.out.println(recvBuf.getLong());
+    memory.deregister();
+    recvMemory.deregister();
   }
 
   private static byte[] getMd5Hash(String object) {
@@ -278,50 +373,65 @@ public class LucyTest {
   };
 
   public void runTestClient() {
-    UcpContext context = new UcpContext(new UcpParams()
-        .requestStreamFeature()
-        .requestTagFeature());
-    UcpWorker worker = context.newWorker(new UcpWorkerParams());
+    UcpWorker worker = globalContext_.newWorker(new UcpWorkerParams());
     // client will receive from server
     UcpEndpoint serverEp = connectToServer(worker);
+    System.out.println("connected: localaddr:" + serverEp.getLocalAddress()
+        + " remote addr:" + serverEp.getRemoteAddress());
 
     FileChannel fileChannel = null;
     String recvFileName = "/tmp/recv_file";
     try {
       fileChannel = FileChannel.open(Paths.get(recvFileName),
-          EnumSet.of(StandardOpenOption.CREATE,
-              StandardOpenOption.TRUNCATE_EXISTING,
-              StandardOpenOption.WRITE)
+          CREATE, WRITE, READ, DELETE_ON_CLOSE
       );
-      MappedByteBuffer buf = fileChannel.map(FileChannel.MapMode.READ_WRITE, 0, MEM_SIZE);
-      UcpMemory mmapedMemory = context.memoryMap(new UcpMemMapParams()
-          .setAddress(UcxUtils.getAddress(buf)).setLength(MEM_SIZE).nonBlocking());
+//      MappedByteBuffer buf = fileChannel.map(FileChannel.MapMode.READ_WRITE, 0, MEM_SIZE);
+      ByteBuffer buf = ByteBuffer.allocateDirect(MEM_SIZE);
+//      UcpMemory mmapedMemory = globalContext_.memoryMap(new UcpMemMapParams()
+//          .setAddress(UcxUtils.getAddress(buf)).setLength(MEM_SIZE).nonBlocking());
 //      UcpRequestParams ucpRequestParams = new UcpRequestParams()
 //          .setMemoryType(UcsConstants.MEMORY_TYPE.UCS_MEMORY_TYPE_HOST);
       long tag = getTag(FILE_TO_SEND, 0);
-      ReentrantLock lock = new ReentrantLock();
-      Condition condition = lock.newCondition();
-      UcpRequest recvReq = worker.recvTaggedNonBlocking(mmapedMemory.getAddress(), MEM_SIZE, tag, tagFileIdMask_,
+//      ReentrantLock lock = new ReentrantLock();
+//      Condition condition = lock.newCondition();
+      UcpRequest recvReq = worker.recvTaggedNonBlocking(
+          UcxUtils.getAddress(buf), MEM_SIZE, tag, tagFileIdMask_,
+//          mmapedMemory.getAddress(), MEM_SIZE, tag, tagFileIdMask_,
           new UcxCallback() {
             final long startTime = System.nanoTime();
 
             @Override
             public void onSuccess(UcpRequest request) {
               System.out.println("onSuccess! request completed:" + request.isCompleted());
-              condition.signalAll();
+//              condition.signalAll();
             }
 
             @Override
             public void onError(int ucsStatus, String errorMsg) {
               logUtil(errorMsg + ",status:" + ucsStatus, null);
-              condition.signalAll();
+//              condition.signalAll();
             }
           });
-      condition.await();
+      while(!recvReq.isCompleted()) {
+        try {
+          System.out.println("start progressing recvReq");
+          worker.progressRequest(recvReq);
+        } catch (Exception e) {
+          logUtil("error progressing recvReq", e);
+          throw new RuntimeException(e);
+        }
+      }
       System.out.println("Waked on waiting, request isCompleted:" + recvReq.isCompleted());
-      mmapedMemory.deregister();
-      context.close();
-    } catch (IOException | InterruptedException e) {
+//      mmapedMemory.deregister();
+      System.out.println("buf remaining:" + buf.remaining()
+          + " buf position:" + buf.position()
+          + " buf limit:" + buf.limit()
+          + " buf capacity:" + buf.capacity());
+      byte[] bytes = new byte[buf.remaining()];
+      buf.get(bytes);
+      System.out.println("Received buffer content:" + new String(bytes));
+      globalContext_.close();
+    } catch (IOException e) {
       logUtil("error open file channel to received file:", e);
     }
   }
--- bindings/java/src/test/java/org/openucx/jucx/UcpLucyTest.java
+++ bindings/java/src/test/java/org/openucx/jucx/UcpLucyTest.java
@@ -6,10 +6,31 @@ import org.openucx.jucx.lucytest.LucyTest;
 public class UcpLucyTest {
 
   @Test
-  public void testLucyTestClient() {
-    LucyTest lucyTestServer = new LucyTest();
-    LucyTest lucyTestClient = new LucyTest();
-    lucyTestServer.runTestServer();
-    lucyTestServer.runTestClient();
+  public void testLucyTestClient() throws InterruptedException {
+    Thread t1 = new Thread(new Runnable() {
+      @Override
+      public void run() {
+        LucyTest lucyTestServer = new LucyTest();
+        try {
+          System.out.println("Start runTestServer...");
+          lucyTestServer.runTestServer();
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+    });
+    Thread t2 = new Thread(new Runnable() {
+      @Override
+      public void run() {
+        System.out.println("Start runTestClient...");
+        LucyTest lucyTestClient = new LucyTest();
+        lucyTestClient.runTestClient();
+      }
+    });
+    t1.start();
+    Thread.sleep(5000);
+    t2.start();
+    t1.join();
+    t2.join();
   }
 }
